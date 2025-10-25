70fef3ccda0a6687754d3c18e5dea1c1be453294
gyubid <84581-gyubid@users.noreply.gitgud.io>
2025-10-25 11:14:34 -0300

    Fix bug

--- HookTN.c
+++ HookTN.c
@@ -141,6 +141,56 @@ sds GetFqnFromClass(Il2CppClass *klass) {
     return res;
 }
 
+re_t GetFuzzyApiPattern(void) {
+    return re_compile("[a-zA-Z]+");
+}
+
+bool RegexEqualMatches(re_t patt, char *s1, char *s2) {
+    int len1 = 0;
+    int len2 = 0;
+    int idx1;
+    int idx2;
+    int it1 = 0;
+    int it2 = 0;
+    do {
+        idx1 = re_matchp(patt, s1 + it1, &len1);
+        idx2 = re_matchp(patt, s2 + it2, &len2);
+        // printf("idx1(%d): %d, idx2(%d): %d\n", len1, idx1, len2, idx2);
+        if (idx1 != -1 && idx2 != -1) {
+            if (len1 != len2) {
+                return false;
+            }
+
+            if (memcmp(s1 + it1 + idx1, s2 + it2 + idx2, len1)) {
+                return false;
+            }
+
+            it1 += len1;
+            it2 += len2;
+        }
+    } while (idx1 != -1 && idx2 != -1);
+
+    if (((idx1 == -1) && (idx2 != -1)) || ((idx1 != -1) && (idx2 == -1))) {
+        return false;
+    }
+
+    return true;
+}
+
+struct ResponseTypeToRequestPtr *findResTypeToReqPtrFromUriFuzzy(char *uri) {
+    re_t pat = GetFuzzyApiPattern();
+
+    struct ResponseTypeToRequestPtr *end = RES_TYPE_TO_REQ_PTR_LIST.data + RES_TYPE_TO_REQ_PTR_LIST.len;
+    struct ResponseTypeToRequestPtr *it;
+    for (it = RES_TYPE_TO_REQ_PTR_LIST.data; it != end; ++it) {
+        if (RegexEqualMatches(pat, uri, it->uriPath)) {
+            return it;
+        }
+    }
+
+    return NULL;
+}
+
 struct ResponseTypeToRequestPtr *findResTypeToReqPtrFromUri(char *uri) {
     struct ResponseTypeToRequestPtr *end = RES_TYPE_TO_REQ_PTR_LIST.data + RES_TYPE_TO_REQ_PTR_LIST.len;
     struct ResponseTypeToRequestPtr *it;
@@ -178,7 +228,7 @@ Il2CppObject *DetourSourceCore_GetResult(
             jsonRes = sdsempty();
         }
 
-        struct ResponseTypeToRequestPtr *resTypeToReqPtr = findResTypeToReqPtrFromUri(neonApiPath);
+        struct ResponseTypeToRequestPtr *resTypeToReqPtr = findResTypeToReqPtrFromUriFuzzy(neonApiPath);
         sds jsonReq;
         if (resTypeToReqPtr) {
             if (resTypeToReqPtr->requestPtr) {
--- test_re.c
+++ test_re.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+#define ARRAY_SIZE(x) (sizeof x/sizeof *x)
 
 char *test_cases[] = {
     "Google.Protobuf.MessageParser`1[Neon.Model.Api.Rpc.AdventureMoveToAreaResponse]",
@@ -12,9 +15,24 @@ char *test_cases[] = {
     "Google.Protobuf.MessageParser`1[Neon.Model.Api.Rpc.AdventureAreaObjectResponse]"
 };
 
+struct PathTestCase {
+    char *s1;
+    char *s2;
+};
+
+struct PathTestCase path_test_cases[] = {
+    {"/field_boss/entry", "/field/boss_entry"},
+    {"/fractal_vise/battle/start", "/fractal_vise/battle_start"},
+    {"/obvious/inequal", "/to/this/path"},
+    {"/another/test", "/another/estt"},
+    {"/another/test", "/another"}
+};
+
 re_t GetNeonModelApiPattern(void) {
     return re_compile("Neon.Model.Api.Rpc.\\w+");
 }
+
+
 
 char *MatchNeonModelApi(re_t patt, char *s) {
     int match_length;
@@ -30,9 +48,43 @@ char *MatchNeonModelApi(re_t patt, char *s) {
     return NULL;
 }
 
-#define ARRAY_SIZE(x) (sizeof x/sizeof *x)
+re_t GetFuzzyApiPattern(void) {
+    return re_compile("[a-zA-Z]+");
+}
 
-int main(void) {
+bool RegexEqualMatches(re_t patt, char *s1, char *s2) {
+    int len1 = 0;
+    int len2 = 0;
+    int idx1;
+    int idx2;
+    int it1 = 0;
+    int it2 = 0;
+    do {
+        idx1 = re_matchp(patt, s1 + it1, &len1);
+        idx2 = re_matchp(patt, s2 + it2, &len2);
+        // printf("idx1(%d): %d, idx2(%d): %d\n", len1, idx1, len2, idx2);
+        if (idx1 != -1 && idx2 != -1) {
+            if (len1 != len2) {
+                return false;
+            }
+
+            if (memcmp(s1 + it1 + idx1, s2 + it2 + idx2, len1)) {
+                return false;
+            }
+
+            it1 += len1;
+            it2 += len2;
+        }
+    } while (idx1 != -1 && idx2 != -1);
+
+    if (((idx1 == -1) && (idx2 != -1)) || ((idx1 != -1) && (idx2 == -1))) {
+        return false;
+    }
+
+    return true;
+}
+
+static void TestNeonModelApiPattern(void) {
     char **it;
     char **end = test_cases + ARRAY_SIZE(test_cases);
 
@@ -43,5 +95,23 @@ int main(void) {
         if (s) {
             puts(s);
         }
-    }    
+    }
+}
+
+static void TestFuzzyPathPattern(void) {
+    struct PathTestCase *it;
+    struct PathTestCase *end = path_test_cases + ARRAY_SIZE(path_test_cases);
+
+    re_t pattern = GetFuzzyApiPattern();
+
+    for (it = path_test_cases; it != end; ++it) {
+        bool res = RegexEqualMatches(pattern, it->s1, it->s2);
+        printf("'%s' %s '%s'\n", it->s1, res ? "==" : "!=", it->s2);
+    }
+}
+
+
+int main(void) {
+    TestNeonModelApiPattern();
+    TestFuzzyPathPattern();
 }
