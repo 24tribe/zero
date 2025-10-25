0cdf1e7157e2132c86cd0d0954b4d70afef1121e
gyubid <84581-gyubid@users.noreply.gitgud.io>
2025-10-25 11:14:27 -0300

    Add Adventure_AccessWarpPoint hook

--- il2cpp_lean.h
+++ il2cpp_lean.h
@@ -2890,4 +2890,65 @@ typedef struct Neon_Model_Api_Rpc_XbStartRequest_o {
 	Neon_Model_Api_Rpc_XbStartRequest_Fields fields;
 } Neon_Model_Api_Rpc_XbStartRequest_o;
 
+typedef struct Cysharp_Threading_Tasks_UniTask_AdventureAccessWarpPointResponse__Fields {
+	struct Cysharp_Threading_Tasks_IUniTaskSource_T__o* source;
+	struct Neon_Model_Api_Rpc_AdventureAccessWarpPointResponse_o* result;
+	int16_t token;
+} Cysharp_Threading_Tasks_UniTask_AdventureAccessWarpPointResponse__Fields;
+
+typedef struct Cysharp_Threading_Tasks_UniTask_AdventureAccessWarpPointResponse__VTable {
+	VirtualInvokeData _0_Equals;
+	VirtualInvokeData _1_Finalize;
+	VirtualInvokeData _2_GetHashCode;
+	VirtualInvokeData _3_ToString;
+} Cysharp_Threading_Tasks_UniTask_AdventureAccessWarpPointResponse__VTable;
+
+typedef struct Cysharp_Threading_Tasks_UniTask_AdventureAccessWarpPointResponse__c {
+	Il2CppClass_1 _1;
+	void* static_fields;
+	Il2CppRGCTXData* rgctx_data;
+	Il2CppClass_2 _2;
+	Cysharp_Threading_Tasks_UniTask_AdventureAccessWarpPointResponse__VTable vtable;
+} Cysharp_Threading_Tasks_UniTask_AdventureAccessWarpPointResponse__c;
+
+typedef struct Cysharp_Threading_Tasks_UniTask_AdventureAccessWarpPointResponse__o {
+	Cysharp_Threading_Tasks_UniTask_AdventureAccessWarpPointResponse__Fields fields;
+} Cysharp_Threading_Tasks_UniTask_AdventureAccessWarpPointResponse__o;
+
+typedef struct Neon_Model_Api_Rpc_AdventureAccessWarpPointRequest_Fields {
+	struct Google_Protobuf_UnknownFieldSet_o* _unknownFields;
+	int32_t warpPointId_;
+	struct Neon_Model_Api_Model_CurrentLocation_o* currentLocation_;
+} Neon_Model_Api_Rpc_AdventureAccessWarpPointRequest_Fields;
+
+typedef struct Neon_Model_Api_Rpc_AdventureAccessWarpPointRequest_VTable {
+	VirtualInvokeData _0_Equals;
+	VirtualInvokeData _1_Finalize;
+	VirtualInvokeData _2_GetHashCode;
+	VirtualInvokeData _3_ToString;
+	VirtualInvokeData _4_MergeFrom;
+	VirtualInvokeData _5_MergeFrom;
+	VirtualInvokeData _6_WriteTo;
+	VirtualInvokeData _7_CalculateSize;
+	VirtualInvokeData _8_pb__Google_Protobuf_IMessage_get_Descriptor;
+	VirtualInvokeData _9_Equals;
+	VirtualInvokeData _10_Clone;
+	VirtualInvokeData _11_pb__Google_Protobuf_IBufferMessage_InternalMergeFrom;
+	VirtualInvokeData _12_pb__Google_Protobuf_IBufferMessage_InternalWriteTo;
+} Neon_Model_Api_Rpc_AdventureAccessWarpPointRequest_VTable;
+
+typedef struct Neon_Model_Api_Rpc_AdventureAccessWarpPointRequest_c {
+	Il2CppClass_1 _1;
+	struct Neon_Model_Api_Rpc_AdventureAccessWarpPointRequest_StaticFields* static_fields;
+	Il2CppRGCTXData* rgctx_data;
+	Il2CppClass_2 _2;
+	Neon_Model_Api_Rpc_AdventureAccessWarpPointRequest_VTable vtable;
+} Neon_Model_Api_Rpc_AdventureAccessWarpPointRequest_c;
+
+typedef struct Neon_Model_Api_Rpc_AdventureAccessWarpPointRequest_o {
+	Neon_Model_Api_Rpc_AdventureAccessWarpPointRequest_c *klass;
+	void *monitor;
+	Neon_Model_Api_Rpc_AdventureAccessWarpPointRequest_Fields fields;
+} Neon_Model_Api_Rpc_AdventureAccessWarpPointRequest_o;
+
 #endif
