e2ea322118cd1ff39008354e2088661d96393a6d
gyubid <84581-gyubid@users.noreply.gitgud.io>
2025-10-25 11:14:25 -0300

    Autogen Xb_Play hook

--- il2cpp_lean.h
+++ il2cpp_lean.h
@@ -2762,4 +2762,68 @@ typedef struct Neon_Model_Api_Rpc_XbUpdateTensionRequest_o {
 	Neon_Model_Api_Rpc_XbUpdateTensionRequest_Fields fields;
 } Neon_Model_Api_Rpc_XbUpdateTensionRequest_o;
 
+typedef struct Cysharp_Threading_Tasks_UniTask_XbPlayResponse__Fields {
+	struct Cysharp_Threading_Tasks_IUniTaskSource_T__o* source;
+	struct Neon_Model_Api_Rpc_XbPlayResponse_o* result;
+	int16_t token;
+} Cysharp_Threading_Tasks_UniTask_XbPlayResponse__Fields;
+
+typedef struct Cysharp_Threading_Tasks_UniTask_XbPlayResponse__VTable {
+	VirtualInvokeData _0_Equals;
+	VirtualInvokeData _1_Finalize;
+	VirtualInvokeData _2_GetHashCode;
+	VirtualInvokeData _3_ToString;
+} Cysharp_Threading_Tasks_UniTask_XbPlayResponse__VTable;
+
+typedef struct Cysharp_Threading_Tasks_UniTask_XbPlayResponse__c {
+	Il2CppClass_1 _1;
+	void* static_fields;
+	Il2CppRGCTXData* rgctx_data;
+	Il2CppClass_2 _2;
+	Cysharp_Threading_Tasks_UniTask_XbPlayResponse__VTable vtable;
+} Cysharp_Threading_Tasks_UniTask_XbPlayResponse__c;
+
+typedef struct Cysharp_Threading_Tasks_UniTask_XbPlayResponse__o {
+	Cysharp_Threading_Tasks_UniTask_XbPlayResponse__Fields fields;
+} Cysharp_Threading_Tasks_UniTask_XbPlayResponse__o;
+
+typedef struct Neon_Model_Api_Rpc_XbPlayRequest_Fields {
+	struct Google_Protobuf_UnknownFieldSet_o* _unknownFields;
+	int32_t xbId_;
+	int32_t zoneAreaIndex_;
+	int32_t commandId_;
+	struct System_Nullable_int__o magicOrbId_;
+	bool isAtBatSkip_;
+} Neon_Model_Api_Rpc_XbPlayRequest_Fields;
+
+typedef struct Neon_Model_Api_Rpc_XbPlayRequest_VTable {
+	VirtualInvokeData _0_Equals;
+	VirtualInvokeData _1_Finalize;
+	VirtualInvokeData _2_GetHashCode;
+	VirtualInvokeData _3_ToString;
+	VirtualInvokeData _4_MergeFrom;
+	VirtualInvokeData _5_MergeFrom;
+	VirtualInvokeData _6_WriteTo;
+	VirtualInvokeData _7_CalculateSize;
+	VirtualInvokeData _8_pb__Google_Protobuf_IMessage_get_Descriptor;
+	VirtualInvokeData _9_Equals;
+	VirtualInvokeData _10_Clone;
+	VirtualInvokeData _11_pb__Google_Protobuf_IBufferMessage_InternalMergeFrom;
+	VirtualInvokeData _12_pb__Google_Protobuf_IBufferMessage_InternalWriteTo;
+} Neon_Model_Api_Rpc_XbPlayRequest_VTable;
+
+typedef struct Neon_Model_Api_Rpc_XbPlayRequest_c {
+	Il2CppClass_1 _1;
+	struct Neon_Model_Api_Rpc_XbPlayRequest_StaticFields* static_fields;
+	Il2CppRGCTXData* rgctx_data;
+	Il2CppClass_2 _2;
+	Neon_Model_Api_Rpc_XbPlayRequest_VTable vtable;
+} Neon_Model_Api_Rpc_XbPlayRequest_c;
+
+typedef struct Neon_Model_Api_Rpc_XbPlayRequest_o {
+	Neon_Model_Api_Rpc_XbPlayRequest_c *klass;
+	void *monitor;
+	Neon_Model_Api_Rpc_XbPlayRequest_Fields fields;
+} Neon_Model_Api_Rpc_XbPlayRequest_o;
+
 #endif
