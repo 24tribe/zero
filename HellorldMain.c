acdd966e1bfb5c5b97d046949831e9c816160898
gyubid <84581-gyubid@users.noreply.gitgud.io>
2025-10-25 11:13:48 -0300

    Remove unused files

--- HellorldMain.c
+++
@@ -1,9 +0,0 @@
-#include "RelocationTest.h"
-
-#include <stdio.h>
-
-int main(void) {
-    PrintHellorld();
-    SetHellorld(10);
-    PrintHellorld();
-}
--- RelocationTest.h
+++
@@ -1,8 +0,0 @@
-#ifndef RELOCATION_TEST_H_11_23_PM
-#define RELOCATION_TEST_H_11_23_PM
-
-extern void SetHellorld(int x);
-
-extern void PrintHellorld(void);
-
-#endif
