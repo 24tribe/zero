605458f334afd0211cc89a289f11d3f1a63c4232
gyubid <84581-gyubid@users.noreply.gitgud.io>
2025-10-25 11:13:41 -0300

    yaay

--- hello.c
+++ hello.c
@@ -8,63 +8,63 @@ char evilDLL[] = "D:\\tribenine\\dllinj\\mylib.dll";
 unsigned int evilLen = sizeof(evilDLL) + 1;
 
 int main(int argc, char* argv[]) {
-  HANDLE ph; // process handle
-  HANDLE rt; // remote thread
-  LPVOID rb; // remote buffer
+    HANDLE ph; // process handle
+    HANDLE rt; // remote thread
+    LPVOID rb; // remote buffer
 
-  // handle to kernel32 and pass it to GetProcAddress
-  HMODULE hKernel32 = GetModuleHandle("Kernel32");
+    HMODULE hKernel32 = GetModuleHandle("Kernel32");
 
-  if (!hKernel32) {
-    fputs("GetModuleHandle failed", stderr);
-    return 1;
-  }
+    if (!hKernel32) {
+        fputs("GetModuleHandle failed", stderr);
+        return 1;
+    }
 
-  VOID *lb = GetProcAddress(hKernel32, "LoadLibraryA");
+    VOID *lb = GetProcAddress(hKernel32, "LoadLibraryA");
 
-  if (!lb) {
-    fputs("GetProcAddress failed", stderr);
-    return 1;
-  }
+    if (!lb) {
+        fputs("GetProcAddress failed", stderr);
+        return 1;
+    }
 
-  // parse process ID
-  if ( atoi(argv[1]) == 0) {
-      printf("PID not found :( exiting...\n");
-      return -1;
-  }
-  printf("PID: %i\n", atoi(argv[1]));
-  ph = OpenProcess(PROCESS_ALL_ACCESS, FALSE, (DWORD)atoi(argv[1]));
+    if (argc != 2) {
+        fputs("Usage: hello.exe tribeninepath\n", stdout);
+        return 1;
+    }
 
-  if (!ph) {
-    fputs("OpenProcess failed", stderr);
-    return 1;
-  }
+    char *path = argv[1];
 
-  // allocate memory buffer for remote process
-  rb = VirtualAllocEx(ph, NULL, evilLen, (MEM_RESERVE | MEM_COMMIT), PAGE_EXECUTE_READWRITE);
+    STARTUPINFOA si = {};
+    PROCESS_INFORMATION pi = {};
+    si.cb = sizeof(si);
+    
+    BOOL ret = CreateProcessA(NULL, path, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
 
-  if (!rb) {
-    fputs("VirtualAllocEx failed", stderr);
-    return 1;
-  }
+    if (!ret) {
+        fputs("CreateProcessA failed\n", stderr);
+        return 1;
+    }
 
-  // "copy" evil DLL between processes
-  
+    ph = pi.hProcess;
+    rb = VirtualAllocEx(ph, NULL, evilLen, (MEM_RESERVE | MEM_COMMIT), PAGE_EXECUTE_READWRITE);
 
-  if (!WriteProcessMemory(ph, rb, evilDLL, evilLen, NULL)) {
-    fputs("WriteProcessMemory failed", stderr);
-    return 1;
-  }
+    if (!rb) {
+        fputs("VirtualAllocEx failed", stderr);
+        return 1;
+    }
 
-  // our process start new thread
-  rt = CreateRemoteThread(ph, NULL, 0, (LPTHREAD_START_ROUTINE)lb, rb, 0, NULL);
+    if (!WriteProcessMemory(ph, rb, evilDLL, evilLen, NULL)) {
+        fputs("WriteProcessMemory failed", stderr);
+        return 1;
+    }
 
-  if (!rt) {
-    fputs("CreateRemoteThread failed", stderr);
-    return 1;
-  }
+    rt = CreateRemoteThread(ph, NULL, 0, (LPTHREAD_START_ROUTINE)lb, rb, 0, NULL);
 
-  CloseHandle(ph);
-  return 0;
+    if (!rt) {
+        fputs("CreateRemoteThread failed", stderr);
+        return 1;
+    }
+
+    CloseHandle(ph);
+    return 0;
 }
 
