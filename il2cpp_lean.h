c5596dfb5e7d7db62e2c8b2bd19bb553b1ec7b71
gyubid <84581-gyubid@users.noreply.gitgud.io>
2025-10-25 11:14:24 -0300

    Autogen Xb_Status hook

--- il2cpp_lean.h
+++ il2cpp_lean.h
@@ -2616,4 +2616,64 @@ typedef struct Cysharp_Threading_Tasks_UniTask_XbFormationResponse__o {
 	Cysharp_Threading_Tasks_UniTask_XbFormationResponse__Fields fields;
 } Cysharp_Threading_Tasks_UniTask_XbFormationResponse__o;
 
+typedef struct Neon_Model_Api_Rpc_XbStatusRequest_Fields {
+	struct Google_Protobuf_UnknownFieldSet_o* _unknownFields;
+	int32_t xbId_;
+} Neon_Model_Api_Rpc_XbStatusRequest_Fields;
+
+typedef struct Neon_Model_Api_Rpc_XbStatusRequest_VTable {
+	VirtualInvokeData _0_Equals;
+	VirtualInvokeData _1_Finalize;
+	VirtualInvokeData _2_GetHashCode;
+	VirtualInvokeData _3_ToString;
+	VirtualInvokeData _4_MergeFrom;
+	VirtualInvokeData _5_MergeFrom;
+	VirtualInvokeData _6_WriteTo;
+	VirtualInvokeData _7_CalculateSize;
+	VirtualInvokeData _8_pb__Google_Protobuf_IMessage_get_Descriptor;
+	VirtualInvokeData _9_Equals;
+	VirtualInvokeData _10_Clone;
+	VirtualInvokeData _11_pb__Google_Protobuf_IBufferMessage_InternalMergeFrom;
+	VirtualInvokeData _12_pb__Google_Protobuf_IBufferMessage_InternalWriteTo;
+} Neon_Model_Api_Rpc_XbStatusRequest_VTable;
+
+typedef struct Neon_Model_Api_Rpc_XbStatusRequest_c {
+	Il2CppClass_1 _1;
+	struct Neon_Model_Api_Rpc_XbStatusRequest_StaticFields* static_fields;
+	Il2CppRGCTXData* rgctx_data;
+	Il2CppClass_2 _2;
+	Neon_Model_Api_Rpc_XbStatusRequest_VTable vtable;
+} Neon_Model_Api_Rpc_XbStatusRequest_c;
+
+typedef struct Neon_Model_Api_Rpc_XbStatusRequest_o {
+	Neon_Model_Api_Rpc_XbStatusRequest_c *klass;
+	void *monitor;
+	Neon_Model_Api_Rpc_XbStatusRequest_Fields fields;
+} Neon_Model_Api_Rpc_XbStatusRequest_o;
+
+typedef struct Cysharp_Threading_Tasks_UniTask_XbStatusResponse__Fields {
+	struct Cysharp_Threading_Tasks_IUniTaskSource_T__o* source;
+	struct Neon_Model_Api_Rpc_XbStatusResponse_o* result;
+	int16_t token;
+} Cysharp_Threading_Tasks_UniTask_XbStatusResponse__Fields;
+
+typedef struct Cysharp_Threading_Tasks_UniTask_XbStatusResponse__VTable {
+	VirtualInvokeData _0_Equals;
+	VirtualInvokeData _1_Finalize;
+	VirtualInvokeData _2_GetHashCode;
+	VirtualInvokeData _3_ToString;
+} Cysharp_Threading_Tasks_UniTask_XbStatusResponse__VTable;
+
+typedef struct Cysharp_Threading_Tasks_UniTask_XbStatusResponse__c {
+	Il2CppClass_1 _1;
+	void* static_fields;
+	Il2CppRGCTXData* rgctx_data;
+	Il2CppClass_2 _2;
+	Cysharp_Threading_Tasks_UniTask_XbStatusResponse__VTable vtable;
+} Cysharp_Threading_Tasks_UniTask_XbStatusResponse__c;
+
+typedef struct Cysharp_Threading_Tasks_UniTask_XbStatusResponse__o {
+	Cysharp_Threading_Tasks_UniTask_XbStatusResponse__Fields fields;
+} Cysharp_Threading_Tasks_UniTask_XbStatusResponse__o;
+
 #endif
