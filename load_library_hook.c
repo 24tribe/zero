7686b973c14a6e6847bc2142da31390cd4492d2c
gyubid <84581-gyubid@users.noreply.gitgud.io>
2025-10-25 11:13:42 -0300

    Remove more unused hooks

--- load_library_hook.c
+++ load_library_hook.c
@@ -11,72 +11,20 @@
 
 #include <stdio.h>
 
-struct FileHandle;
-typedef void *(*MEMORYMAPPEDFILEMAP)(struct FileHandle *);
-
-typedef void *(*LOADMETADATAFILE)(const char *fileName);
-
 typedef HMODULE (WINAPI *LOADLIBRARYW)(LPCWSTR);
 
 LOADLIBRARYW fpLoadLibraryW = NULL;
-MEMORYMAPPEDFILEMAP fpMemoryMappedFileMap = NULL;
-LOADMETADATAFILE fpLoadMetadataFile = NULL;
+bool alreadyDumped = false;
 
-
-ptrdiff_t MemoryMappedFileMapOffset = 0x670b90;
-ptrdiff_t LoadMetadataFileOffset = 0x6b0870;
-
-void SaveMetadataFromMem(BYTE *res) {
-    char *path = "D:\\SteamLibrary\\steamapps\\common\\TRIBENINE\\tribenine_Data\\il2cpp_data\\Metadata\\global-metadata.dat";
-    long long size = GetMetadataSize(path);
-
-    if (size == LLONG_MAX) {
-        printf("GetMetadataSize failed\n");
-        return;
-    }
-
-    printf("Size: 0x%lld\n", size);
-    
-    if (size > (long long)LONG_MAX) {
-        printf("Size doesnt fit in a long\n");
-        return;
-    }
-
-    unsigned long smallSize = (unsigned long)size;
-
-    if (SaveMetadata("metadata.dat", (BYTE *)res, smallSize)) {
-        printf("SaveMetadata Success\n");
-    } else {
-        printf("SaveMEtadata Failed\n");
-    }
-}
-
-void *DetourLoadMetadataFile(const char *filename) {
-    printf("LoadMetadataFile(fileName=%s)\n", filename);
-    BYTE *res = fpLoadMetadataFile(filename);
-    if (IsMetadata(res)) {
-        SaveMetadataFromMem(res);
-    }
-    return res;
-}
-
-void *DetourMemoryMappedFileMap(struct FileHandle *fileHandle) {
-    printf("MemoryMappedFileMap called\n");
-    BYTE *res = fpMemoryMappedFileMap(fileHandle);
-    return res;
-}
-
-void HookMemoryMappedFileMap(HMODULE GameAssembly) {
+void DumpGameAssembly(HMODULE GameAssembly) {
     unsigned long long GameAssemblySize = CalcDLLSize(GameAssembly);
 
-    
     if (SaveMetadata("ga.dump", (void *)GameAssembly, GameAssemblySize)) {
         fputs("Saved ga.dump\n", stdout);
     } else {
         fputs("Failed to save ga.dump\n", stdout);
     }
-    
-    
+        
     InitRemapMem();
 
     if (RemapViewOfSection(GetCurrentProcess(), (void*)GameAssembly, GameAssemblySize, PAGE_EXECUTE_READWRITE)) {
@@ -85,50 +33,13 @@ void HookMemoryMappedFileMap(HMODULE GameAssembly) {
         printf("Remap Failed!\n");
         return;
     }
-
-    MEMORYMAPPEDFILEMAP original = (void *)((char *)GameAssembly + MemoryMappedFileMapOffset);
-    LOADMETADATAFILE xLoadMetadataFile = (void *)((char *)GameAssembly + LoadMetadataFileOffset);
-
-    printf("MemoryMappedFileMap address: %p\n", (void *)original);
-    printf("LoadMetadataFile address: %p\n", (void *)xLoadMetadataFile);
-
-    int status = 0;
-    if ((status = MH_CreateHook(original, &DetourMemoryMappedFileMap,
-        (LPVOID *)(&fpMemoryMappedFileMap))) != MH_OK) {
-        printf("Failed to create MemoryMappedFileMap hook: %s\n", MH_StatusToString(status));
-        return;
-    }
-
-    if ((status = MH_EnableHook(original, FALSE)) != MH_OK) {
-        printf("failed to enable MemoryMappedFileMap hook: %s\n", MH_StatusToString(status));
-        return;
-    }
-
-    if ((status = MH_CreateHook(xLoadMetadataFile, &DetourLoadMetadataFile,
-        (LPVOID *)(&fpLoadMetadataFile))) != MH_OK) {
-        printf("Failed to create LoadMetadataFile hook: %s\n", MH_StatusToString(status));
-        return;
-    }
-
-    if ((status = MH_EnableHook(xLoadMetadataFile, FALSE)) != MH_OK) {
-        printf("failed to enable LoadMetadataFile hook: %s\n", MH_StatusToString(status));
-        return;
-    }
 }
 
-// #define HOOK_STEAMAPI
 HMODULE WINAPI DetourLoadLibraryW(LPCWSTR s) {
     char news[MY_LINE_SIZE];
     int len = WideCharToMultiByte(CP_UTF8, 0, s, -1, 0, 0, 0, 0);
     WideCharToMultiByte(CP_UTF8, 0, s, -1, news, len, 0, 0);
     printf("LoadLibraryW: %s\n", news);
-
-    #ifdef HOOK_STEAMAPI
-    if (strstr(news, "steam_api64.dll")) {
-        puts("hooking steam_api64.dll");
-        return fpLoadLibraryW(L"D:\\tribenine\\dllinj\\steam_api64.dll");
-    }
-    #endif
 
     BOOL isGameAssembly = strstr(news, "GameAssembly.dll") ? TRUE : FALSE;
 
@@ -136,9 +47,10 @@ HMODULE WINAPI DetourLoadLibraryW(LPCWSTR s) {
 
     if (isGameAssembly) {
         printf("GameAssembly location: %p\n", (void *)res);
-        if (!fpMemoryMappedFileMap) {
+        if (!alreadyDumped) {
+            alreadyDumped = true;
             press_enter_to_continue();
-            HookMemoryMappedFileMap(res);
+            DumpGameAssembly(res);
             press_enter_to_continue();
         }
     }
