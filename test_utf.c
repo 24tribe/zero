535ae537db24586212f807cd21b9aae2741aa26d
gyubid <84581-gyubid@users.noreply.gitgud.io>
2025-10-27 18:15:10 -0300

    Fix test

--- test_utf.c
+++ test_utf.c
@@ -7,9 +7,10 @@ int main(void) {
     uint16_t data[] = u"みなも";
     uint32_t data_len = sizeof data/sizeof *data;
     sds s = sds16to8(data, data_len);
-    while (*s) {
-        printf("%02x", (unsigned int)(unsigned char)*s++);
+    char *it = s;
+    while (*it) {
+        printf("%02x", (unsigned int)(unsigned char)*it++);
     }
     printf("\n");
-    // sdsfree(s); // crash?
+    sdsfree(s);
 }
