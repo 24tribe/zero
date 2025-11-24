347142e5fffc202432a0a2892c99af10a6e2d5ef
gyubid <84581-gyubid@users.noreply.gitgud.io>
2025-11-24 18:38:02 -0300

    Remove unused batch script

--- TRIBENINE.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set LOADER_PATH=%~dp0
-set ZERO_DLL_PATH=%LOADER_PATH%\libzero.dll
-set TRIBENINE_PATH=E:\SteamLibrary\steamapps\common\TRIBENINE
-
-cd /D "%LOADER_PATH%"
-.\loader.exe "%TRIBENINE_PATH%\tribenine.exe" "%ZERO_DLL_PATH%"
