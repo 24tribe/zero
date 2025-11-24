ab329183f1faeb41942fe421a3656eda6371bce7
gyubid <84581-gyubid@users.noreply.gitgud.io>
2025-11-24 17:43:43 -0300

    Get path of tribe nine executable

--- launcher/launcher.cpp
+++ launcher/launcher.cpp
@@ -1,11 +1,25 @@
 #include "webview/webview.h"
 #include <stddef.h>
 
+#include <jansson.h>
+
 #include <windows.h>
+#include <commdlg.h>
 
 #include <sstream>
+#include <iostream>
 
 #define CURRENT_DIR_SIZE 4096
+#define EXE_PATH_SIZE 4096
+
+static void initOpenInfo(OPENFILENAMEA* openInfo, char *exePath) {
+  ZeroMemory(openInfo, sizeof(*openInfo));
+  openInfo->lStructSize = sizeof(*openInfo);
+  openInfo->lpstrFilter = "TRIBE NINE executable\0tribenine.exe\0\0";
+  openInfo->lpstrFile = exePath;
+  openInfo->nMaxFile = EXE_PATH_SIZE;
+  openInfo->Flags = OFN_FILEMUSTEXIST;
+}
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine,
                    int nCmdShow) {
@@ -16,6 +30,26 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine,
   webview_t w = webview_create(1, NULL);
   webview_set_title(w, "Tribe Nine Launcher");
   webview_set_size(w, 720, 480, WEBVIEW_HINT_NONE);
+
+  webview_bind(w, "selectTribeNineExePath", [](const char *id, const char *req, void *arg) {
+    (void)req;
+    webview_t wv = reinterpret_cast<webview_t>(arg);
+    OPENFILENAMEA openInfo;
+    char exePath[EXE_PATH_SIZE] = {0};
+    initOpenInfo(&openInfo, exePath);
+
+    // FIXME: this should be GetOpenFileNameW
+    if (GetOpenFileNameA(&openInfo)) {
+      json_t *value = json_string(exePath);
+      char *s = json_dumps(value, JSON_ENCODE_ANY);
+      webview_return(wv, id, 0, s);
+      free(s);
+      json_decref(value);
+    } else {
+      webview_return(wv, id, 1, "");
+    }
+  }, w);
+
   char current_dir[CURRENT_DIR_SIZE];
   // FIXME: this should be GetCurrentDirectoryW
   if (GetCurrentDirectoryA(CURRENT_DIR_SIZE, current_dir) == 0) {
