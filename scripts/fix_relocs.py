4a11d51fa4944a4ad61832c9d88014377da023e1
gyubid <84581-gyubid@users.noreply.gitgud.io>
2025-11-28 10:45:34 -0300

    Don't hardcode the dll base

--- scripts/fix_relocs.py
+++ scripts/fix_relocs.py
@@ -1,32 +1,39 @@
 from argparse import ArgumentParser
 import struct
- 
+
+def get_higher_limit(base_address):
+    base_address &= 0xffff00000000
+    base_address += 0x0000f0000000
+    return base_address
+
 def main():
+    assert get_higher_limit(0x7ffcc2850000) == 0x7ffcf0000000
+
     parser = ArgumentParser()
     parser.add_argument("in_bin")
     parser.add_argument("out_bin")
+    parser.add_argument("base_address", type=int)
     args = parser.parse_args()
     
     with open(args.in_bin, "rb") as f:
         data = bytearray(f.read())
 
-    base_address = 0x7ffcc2850000
     new_base_address = 0x180000000
 
     with open(args.in_bin, "rb") as infile:
         with open(args.out_bin, "wb") as out:
             while data := infile.read(8):
                 addr = struct.unpack("<Q", data)[0]
-                
-                if 0x7ffcf0000000 >= addr >= 0x7ffcc2850000:
-                    new_addr = new_base_address + (addr - base_address)
+
+                if args.base_address <= addr <= get_higher_limit(args.base_address):
+                    new_addr = new_base_address + (addr - args.base_address)
                 else:
                     new_addr = addr
-                
+
                 enc_addr = struct.pack("<Q", new_addr)
 
                 out.write(enc_addr)
-    
+
     print("OK")
 
         
