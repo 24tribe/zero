ddc3e6c05bbf65de5ae937f3c69d64fa9fb51d6c
gyubid <84581-gyubid@users.noreply.gitgud.io>
2025-11-29 13:07:52 -0300

    Try to hook HTTPRequest ctor
    But failed with this error message:
    
    ```
    [13:03:53.761] AccessTools.DeclaredMethod: Could not find method for type Il2CppBest.HTTP.HTTPRequest and name .ctor and parameters
    [13:03:53.767] [ZeroMod] HarmonyLib.HarmonyException: Patching exception in method null
     ---> System.ArgumentException: Undefined target method for patch method static void zeromod.ZeroMod+DetourHTTPRequestCtor::Prefix(Uri& uri, Il2CppBest.HTTP.HTTPMethods method)
       at HarmonyLib.PatchClassProcessor.PatchWithAttributes(MethodBase& lastOriginal)
       at HarmonyLib.PatchClassProcessor.Patch()
       --- End of inner exception stack trace ---
       at HarmonyLib.PatchClassProcessor.ReportException(Exception exception, MethodBase original)
       at HarmonyLib.PatchClassProcessor.Patch()
       at HarmonyLib.Harmony.<PatchAll>b__11_0(Type type)
       at HarmonyLib.CollectionExtensions.Do[T](IEnumerable`1 sequence, Action`1 action)
       at HarmonyLib.Harmony.PatchAll(Assembly assembly)
       at MelonLoader.MelonMod.HarmonyInit() in D:\a\MelonLoader\MelonLoader\MelonLoader\Melons\MelonMod.cs:line 40
       at MelonLoader.MelonEvent.<>c.<Invoke>b__1_0(LemonAction x) in D:\a\MelonLoader\MelonLoader\MelonLoader\Melons\Events\MelonEvent.cs:line 174
       at MelonLoader.MelonEventBase`1.Invoke(Action`1 delegateInvoker) in D:\a\MelonLoader\MelonLoader\MelonLoader\Melons\Events\MelonEvent.cs:line 143
    ```
    
    The alternative is to use [native
    hooking](https://melonwiki.xyz/#/modders/patching?id=patching-using-native-hooks)
    but is not worth it IMO, I'll just give up and adapt the C source to the
    open beta.

--- zeromod/ZeroMod.cs
+++ zeromod/ZeroMod.cs
@@ -1,4 +1,6 @@
 ﻿using MelonLoader;
+using Il2CppBest.HTTP;
+using HarmonyLib;
 
 [assembly: MelonInfo(typeof(zeromod.ZeroMod), "ZeroMod", "0.1.0", "TNZ contributors")]
 [assembly: MelonGame("Akatsuki Games Inc.", "tribenine")]
@@ -7,8 +9,21 @@ namespace zeromod;
 
 public class ZeroMod : MelonMod
 {
+    private static MelonLogger.Instance? logger;
+
+    [HarmonyPatch(typeof(HTTPRequest), ".ctor", new Type[] { typeof(Uri), typeof(HTTPMethods) })]
+    private static class DetourHTTPRequestCtor
+    {
+        private static void Prefix(ref Uri uri, HTTPMethods method) {
+            if (logger != null) {
+                logger.Msg("DetourHTTPRequestCtor called!");
+            }
+            uri = new Uri("https://httpbin.org/status/500");
+        }
+    }
+
     public override void OnInitializeMelon()
     {
-        LoggerInstance.Msg("Hello World!");
+        logger = LoggerInstance;
     }
 }
