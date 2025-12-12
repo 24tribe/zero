e59f89e775ac00a03293c6812371cfadb38ee4a9
gyubid <84581-gyubid@users.noreply.gitgud.io>
2025-12-11 21:18:51 -0300

    Use work area dimensions to create test window

--- imguitest.cpp
+++ imguitest.cpp
@@ -30,10 +30,26 @@ int main(int, char**)
     ImGui_ImplWin32_EnableDpiAwareness();
     float main_scale = ImGui_ImplWin32_GetDpiScaleForMonitor(::MonitorFromPoint(POINT{ 0, 0 }, MONITOR_DEFAULTTOPRIMARY));
 
+    RECT work_area;
+    ZeroMemory(&work_area, sizeof(work_area));
+
+    if (!SystemParametersInfoA(SPI_GETWORKAREA, 0, &work_area, 0)) {
+        std::cout << "SystemParametersInfoA failed\n";
+        return 1;
+    }
+
+    LONG width = work_area.right;
+    LONG height = work_area.bottom;
+
     // Create application window
     WNDCLASSEXW wc = { sizeof(wc), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(nullptr), nullptr, nullptr, nullptr, nullptr, L"ImGui Example", nullptr };
     ::RegisterClassExW(&wc);
-    HWND hwnd = ::CreateWindowW(wc.lpszClassName, L"Dear ImGui DirectX11 Example", WS_OVERLAPPEDWINDOW, 100, 100, (int)(800 * main_scale), (int)(600 * main_scale), nullptr, nullptr, wc.hInstance, nullptr);
+    HWND hwnd = ::CreateWindowW(
+        wc.lpszClassName, L"Dear ImGui DirectX11 Example", WS_OVERLAPPEDWINDOW,
+        0, 0,
+        (int)(width * main_scale), (int)(height * main_scale),
+        nullptr, nullptr, wc.hInstance, nullptr
+    );
 
     // Initialize Direct3D
     if (!CreateDeviceD3D(hwnd))
