c0c120a73408b740d241fb76322f8b1dc78483b3
gyubid <84581-gyubid@users.noreply.gitgud.io>
2026-01-04 23:30:29 -0300

    Move parsing to parse_class

--- scripts/protobufDump.py
+++ scripts/protobufDump.py
@@ -10,21 +10,38 @@ def main():
     with open(args.cs_file, "r", encoding="utf-8") as f:
         code = f.read()
 
+    class_ = parse_class(code)
+
+    print(f"{class_["class_name"]}\n")
+
+    for field in class_["fields"]:
+        print(f"[ProtoMember({field["proto_field_num"]})]")
+        print(f"{field["field_name"]}: {field["type"]}\n")
+
+def parse_class(code):
     match = re.search(r"public sealed class ([^\s]+)", code)
     assert match
     class_name = match.group(1)
 
-    print(f"{class_name}\n")
+    fields = []
 
     for fieldName, protoFieldNum in re.findall(r"([^\s]+)FieldNumber = (\d+)", code):
         fieldName = fieldName[:1].lower() + fieldName[1:]
-        print(f"[ProtoMember({protoFieldNum})]")
 
         match = re.search(r"private (?:[^\s]+ )*([^\s]+) " + f"{fieldName}_;", code)
         assert match
         type_ = match.group(1)
 
-        print(f"{fieldName}: {type_}\n")
+        fields.append({
+            "proto_field_num": protoFieldNum,
+            "field_name": fieldName,
+            "type": type_
+        })
+
+    return {
+        "class_name": class_name,
+        "fields": fields
+    }
 
 
 if __name__ == "__main__":
