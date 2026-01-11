6e75aca7ebced6f2babfe9f9410079c128bbebf6
gyubid <84581-gyubid@users.noreply.gitgud.io>
2026-01-10 22:18:26 -0300

    Add a fixed response to news UserList and List

--- semba/sembastable.nim
+++ semba/sembastable.nim
@@ -2168,6 +2168,36 @@ proc adventure_AccessWarpPoint(db: DbConn, jsonReq: JsonNode): JsonNode =
     }
   }
 
+proc news_UserList(): JsonNode =
+  result = %*{
+    "news": [
+      {
+        "newsGroupId": 1000516,
+        "priority": 1000584,
+        "category": 1,
+        "title": "[Updated 4/24] Instructions for Adjusting Graphic Settings",
+        "startAt": "2025-04-24T08:00:00Z",
+        "editedAt": "2025-04-18T03:00:00Z"
+      },
+      {
+        "newsGroupId": 1002945,
+        "priority": 1000270,
+        "category": 4,
+        "title": "Standard Synchro \"Embarking Companionship\" - Now Available!",
+        "startAt": "2025-04-18T03:00:00Z",
+        "editedAt": "2025-02-26T07:00:00Z"
+      },
+      {
+        "newsGroupId": 1002955,
+        "priority": 1003014,
+        "category": 2,
+        "title": "[Updated 4/25]  Regarding Currently Known Issues",
+        "startAt": "2025-04-25T11:25:00Z",
+        "editedAt": "2025-02-20T02:00:00Z"
+      }
+    ]
+  }
+
 proc getJsonResultStable*(
   uri: string, jsonReq: JsonNode,
   db: DbConn, lastBattleStartReq: var BattleStartRequest
@@ -2227,5 +2257,9 @@ proc getJsonResultStable*(
     result = adventure_Hospital(db)
   elif uri == "/adventure/access_warp_point":
     result = adventure_AccessWarpPoint(db, jsonReq)
+  elif uri == "/news/user_list":
+    result = news_UserList()
+  elif uri == "/news/list":
+    result = news_UserList()
   else:
     result = nil
