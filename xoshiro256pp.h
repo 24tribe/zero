f7249df706eb812518b2033c794bb0c5d3af28cf
gyubid <245973647+gyubid@users.noreply.github.com>
2026-01-26 21:24:52 -0300

    Move xoshiro256pp to semba dir

--- xoshiro256pp.h
+++
@@ -1,9 +0,0 @@
-#ifndef XOSHIRO256PP_H_09_11_2025_11_42_AM
-#define XOSHIRO256PP_H_09_11_2025_11_42_AM
-
-#include <stdint.h>
-
-void xoshiro256pp_set_state(uint64_t s0, uint64_t s1, uint64_t s2, uint64_t s3);
-uint64_t xoshiro256pp_next(void);
-
-#endif
