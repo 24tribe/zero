179d1123e9389a9c8eb25a1527c72526a3b5d22f
gyubid <245973647+gyubid@users.noreply.github.com>
2026-01-26 23:50:01 -0300

    Move SembaDeleteSaveFile to SembaCall

--- semba/sembaprivate.nim
+++ semba/sembaprivate.nim
@@ -28,6 +28,13 @@ proc semba_CreateSaveFile(jsonReq: JsonNode, db: DbConn): JsonNode =
   }
 
 
+proc semba_DeleteSaveFile(jsonReq: JsonNode) =
+  let saves_dir = jsonReq["saves_dir"].getStr()
+  let name = jsonReq["name"].getStr()
+
+  deleteSaveFile(saves_dir, name)
+
+
 proc getJsonResultPrivateApi*(uri: string, jsonReq: JsonNode, db: DbConn): JsonNode =
   if uri == "/semba/echo":
     let dataUpper = jsonReq["data"].getStr().toUpperAscii()
@@ -36,3 +43,5 @@ proc getJsonResultPrivateApi*(uri: string, jsonReq: JsonNode, db: DbConn): JsonN
     result = semba_LoadSaveFile(jsonReq, db)
   elif uri == "/semba/create_save_file":
     result = semba_CreateSaveFile(jsonReq, db)
+  elif uri == "/semba/delete_save_file":
+    semba_DeleteSaveFile(jsonReq)
