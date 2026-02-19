dd808b8a09a164327126e35b9f2556c81aa87864
gyubid <245973647+gyubid@users.noreply.github.com>
2026-02-18 23:46:35 -0300

    Wrap TextureSwap in a class

--- bundlemod/TextureSwap.cs
+++ bundlemod/TextureSwap.cs
@@ -1,4 +1,6 @@
-﻿using AssetsTools.NET;
+﻿namespace bundlemod;
+
+using AssetsTools.NET;
 using AssetsTools.NET.Extra;
 using AssetsTools.NET.Texture;
 using SixLabors.ImageSharp;
@@ -6,57 +8,50 @@ using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using System.Runtime.CompilerServices;
 
-if (args.Length != 3) {
-    Console.WriteLine("Usage: textureswap bundle_in texture_png bundle_out");
-    return 1;
-}
+public class TextureSwap {
+    public SwapTexture(string bundleInPath, string texturePath, string bundleOutPath) {
+        Console.WriteLine($"bundleInPath: {bundleInPath}");
+        Console.WriteLine($"texturePath: {texturePath}");
+        Console.WriteLine($"bundleOutPath: {bundleOutPath}");
 
-var bundleInPath = args[0];
-var texturePath = args[1];
-var bundleOutPath = args[2];
+        var textureName = Path.GetFileName(texturePath).Split(".")[0];
+        Console.WriteLine($"textureName: {textureName}");
 
-Console.WriteLine($"bundleInPath: {bundleInPath}");
-Console.WriteLine($"texturePath: {texturePath}");
-Console.WriteLine($"bundleOutPath: {bundleOutPath}");
+        var manager = new AssetsManager();
+        var bundleInstance = manager.LoadBundleFile(bundleInPath, true);
+        var bundle = bundleInstance.file;
+        var fileIndex = 0;
+        var assetsFileInstance = manager.LoadAssetsFileFromBundle(bundleInstance, fileIndex, false);
+        var assetsFile = assetsFileInstance.file;
 
-var textureName = Path.GetFileName(texturePath).Split(".")[0];
-Console.WriteLine($"textureName: {textureName}");
+        foreach (var textureInfo in assetsFile.GetAssetsOfType(AssetClassID.Texture2D)) {
+            var textureBase = manager.GetBaseField(assetsFileInstance, textureInfo);
+            if (textureBase["m_Name"].AsString == textureName) {
+                Console.WriteLine("Found!!");
+                var texture = TextureFile.ReadTextureFile(textureBase);
+                Console.WriteLine($"{(TextureFormat)texture.m_TextureFormat}");
+                var rawTexDat = texture.FillPictureData(assetsFileInstance);
+                var texDat = texture.DecodeTextureRaw(rawTexDat);
+                var image = Image.LoadPixelData<Bgra32>(texDat, texture.m_Width, texture.m_Height);
+                image.Mutate(i => i.Rotate(RotateMode.Rotate90));
+                image.SaveAsPng("texture.png");
 
-var manager = new AssetsManager();
-var bundleInstance = manager.LoadBundleFile(bundleInPath, true);
-var bundle = bundleInstance.file;
-var fileIndex = 0;
-var assetsFileInstance = manager.LoadAssetsFileFromBundle(bundleInstance, fileIndex, false);
-var assetsFile = assetsFileInstance.file;
+                byte[] pixelBytes = new byte[image.Width * image.Height * Unsafe.SizeOf<Rgba32>()];
+                image.CopyPixelDataTo(pixelBytes);
 
-foreach (var textureInfo in assetsFile.GetAssetsOfType(AssetClassID.Texture2D)) {
-    var textureBase = manager.GetBaseField(assetsFileInstance, textureInfo);
-    if (textureBase["m_Name"].AsString == textureName) {
-        Console.WriteLine("Found!!");
-        var texture = TextureFile.ReadTextureFile(textureBase);
-        Console.WriteLine($"{(TextureFormat)texture.m_TextureFormat}");
-        var rawTexDat = texture.FillPictureData(assetsFileInstance);
-        var texDat = texture.DecodeTextureRaw(rawTexDat);
-        var image = Image.LoadPixelData<Bgra32>(texDat, texture.m_Width, texture.m_Height);
-        image.Mutate(i => i.Rotate(RotateMode.Rotate90));
-        image.SaveAsPng("texture.png");
+                texture.m_TextureFormat = (int)TextureFormat.BGRA32;
+                texture.EncodeTextureRaw(pixelBytes, texture.m_Width, texture.m_Height);
+                texture.WriteTo(textureBase);
 
-        byte[] pixelBytes = new byte[image.Width * image.Height * Unsafe.SizeOf<Rgba32>()];
-        image.CopyPixelDataTo(pixelBytes);
+                textureInfo.SetNewData(textureBase);
+                break;
+            }
+        }
 
-        texture.m_TextureFormat = (int)TextureFormat.BGRA32;
-        texture.EncodeTextureRaw(pixelBytes, texture.m_Width, texture.m_Height);
-        texture.WriteTo(textureBase);
+        bundle.BlockAndDirInfo.DirectoryInfos[fileIndex].SetNewData(assetsFile);
 
-        textureInfo.SetNewData(textureBase);
-        break;
+        using (AssetsFileWriter writer = new AssetsFileWriter(bundleOutPath)) {
+            bundleInstance.file.Write(writer);
+        }
     }
 }
-
-bundle.BlockAndDirInfo.DirectoryInfos[fileIndex].SetNewData(assetsFile);
-
-using (AssetsFileWriter writer = new AssetsFileWriter(bundleOutPath)) {
-    bundleInstance.file.Write(writer);
-}
-
-return 0;
