5dbee28ae07015c1c23904b43d21cad63bad7a3e
gyubid <245973647+gyubid@users.noreply.github.com>
2026-02-19 20:58:50 -0300

    Move ChangeTextures prototype to other file

---
+++ bundlemod/BundleMod.h
@@ -0,0 +1,10 @@
+#ifndef BUNDLE_MOD_H_19_2026_02_08_52
+#define BUNDLE_MOD_H_19_2026_02_08_52
+
+#include <windows.h>
+
+extern void BundleMod_ChangeTextures(
+    HANDLE fromFile, HANDLE toFile, const char *textureChanges
+);
+
+#endif
