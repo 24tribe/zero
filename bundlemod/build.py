9dc30727807d8495eefe21aff5a5ccfad4ffa6ae
gyubid <245973647+gyubid@users.noreply.github.com>
2026-02-20 00:01:22 -0300

    Change Hina's hair color based on imgui window

---
+++ bundlemod/build.py
@@ -0,0 +1,18 @@
+from argparse import ArgumentParser
+import os.path
+import subprocess
+import shutil
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("out_dll")
+    parser.add_argument("project_path")
+    args = parser.parse_args()
+
+    out_dll = os.path.realpath(args.out_dll)
+    os.chdir(args.project_path)
+    subprocess.run(["dotnet", "publish"])
+    shutil.copy("bin/Release/net9.0/win-x64/publish/bundlemod.dll", out_dll)
+
+if __name__ == "__main__":
+    main()
