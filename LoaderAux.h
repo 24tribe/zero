ca3b4207e38f419c14c34903c46f41a71db7a672
gyubid <245973647+gyubid@users.noreply.github.com>
2026-03-08 13:53:46 -0300

    Use windows unicode functions in loader

--- LoaderAux.h
+++ LoaderAux.h
@@ -1,6 +1,9 @@
 #ifndef LOADER_AUX_H_202601090813PM
 #define LOADER_AUX_H_202601090813PM
 
+#include <wchar.h>
+
+int LoadGameEx(const wchar_t *exe_path, const wchar_t *dll_path);
 int LoadGame(const char *path, const char *dll_path);
 
 #endif
--- loader.c
+++ loader.c
@@ -2,15 +2,15 @@
 
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
+int wmain(int argc, wchar_t* argv[]) {
     if (argc != 3) {
         fputs("Usage: Loader.exe game_exe_path dll_path\n", stdout);
         return 1;
     }
 
-    char *path = argv[1];
-    char *dll_path = argv[2];
+    wchar_t *path = argv[1];
+    wchar_t *dll_path = argv[2];
     
-    return LoadGame(path, dll_path);
+    return LoadGameEx(path, dll_path);
 }
 
