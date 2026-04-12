93dc45d8e451c06edda996dafb345d244d08d95c
gyubid <245973647+gyubid@users.noreply.github.com>
2026-04-11 23:29:32 -0300

    Move more functions

--- gui/SembaClient.cpp
+++ gui/SembaClient.cpp
@@ -1,5 +1,7 @@
 #include "SembaClient.h"
 
+#include <iostream>
+
 json_t* encode_semba_mail_gear_request(
     int rarity, int piece, int set, int tier, int substat1, int substat2, int substat3
 ) {
@@ -36,4 +38,112 @@ std::string createSaveReq(const char* saves_dir, const char *name) {
     std::string result = req_s;
     free(req_s);
     return result;
+}
+
+std::pair<int, std::string> createListSaveFilesRequest(const std::string& savesDir) {
+    json_t *req = json_object();
+
+    if (!req) {
+        return std::make_pair(-1, std::string("json_object() failed!"));
+    }
+
+    json_object_set_new(req, "savesDir", json_string(savesDir.c_str()));
+
+    char *reqStr = json_dumps(req, 0);
+
+    if (!reqStr) {
+        return std::make_pair(-1, std::string("json_dumps failed!"));
+    }
+
+    std::string result = reqStr;
+
+    ::free(reqStr);
+
+    return std::make_pair(0, result);
+}
+
+std::pair<int, std::string> wrapSkipTutorial(bool skipTutorial) {
+    json_t* req = json_object();
+
+    if (!req) {
+        return std::make_pair(-1, "json_object() failed!");
+    }
+
+    json_object_set_new(req, "skipTutorial", json_boolean(skipTutorial));
+
+    char *reqStr = json_dumps(req, 0);
+
+    if (!reqStr) {
+        return std::make_pair(-1, "json_dumps failed!");
+    }
+
+    std::string reqResult(reqStr);
+    ::free(reqStr);
+
+    return std::make_pair(0, reqResult);
+}
+
+std::pair<int, std::string> parseGetSkipTutorialResponse(std::string response, bool& skipTutorial) {
+    json_t *resJson = json_loads(response.c_str(), 0, NULL);
+
+    if (!resJson) {
+        return std::make_pair(-1, std::string("json_loads failed!"));
+    }
+
+    json_t *skipTutorialJson = json_object_get(resJson, "skipTutorial");
+
+    if (!skipTutorialJson) {
+        return std::make_pair(-1, std::string("skipTutorial field missing from response"));
+    }
+
+    skipTutorial = json_boolean_value(skipTutorialJson);
+
+    json_decref(resJson);
+
+    return std::make_pair(0, std::string());
+}
+
+std::pair<int, std::string> parseListSaveFilesResponse(
+    const std::string& response, std::vector<std::string>& save_files
+) {
+    json_t *resJson = json_loads(response.c_str(), 0, NULL);
+
+    if (!resJson) {
+        return std::make_pair(-1, std::string("json_loads failed!"));
+    }
+
+    json_t *names = json_object_get(resJson, "names");
+
+    if (!names) {
+        return std::make_pair(-1, std::string("failed to get names field from res"));
+    }
+
+    for (size_t i = 0; i < json_array_size(names); ++i) {
+        json_t *name = json_array_get(names, i);
+
+        save_files.push_back(json_string_value(name));
+    }
+
+    return std::make_pair(0, std::string());
+}
+
+void updateHairColorMapWithHairColors(CharHairColorMap& hairColorMap, json_t *hair_colors) {
+    for (size_t i = 0; i < json_array_size(hair_colors); ++i) {
+        json_t *hair_color = json_array_get(hair_colors, i);
+
+        int charId = json_integer_value(json_object_get(hair_color, "charId"));
+        float r = json_real_value(json_object_get(hair_color, "r"));
+        float g = json_real_value(json_object_get(hair_color, "g"));
+        float b = json_real_value(json_object_get(hair_color, "b"));
+        bool enabled = json_boolean_value(json_object_get(hair_color, "enabled"));
+
+        if (auto search = hairColorMap.find(charId); search != hairColorMap.end()) {
+            search->second.hairColor[0] = r;
+            search->second.hairColor[1] = g;
+            search->second.hairColor[2] = b;
+            search->second.enable = enabled;
+        } else {
+            std::cout << "Warning: charId=" << charId << " not found is hairColorMap\n";
+        }
+    }
 }
--- gui/SembaClient.h
+++ gui/SembaClient.h
@@ -5,6 +5,7 @@
 #include <jansson.h>
 
 #include <string>
+#include <vector>
 
 json_t* encode_semba_mail_gear_request(
     int rarity, int piece, int set, int tier, int substat1, int substat2, int substat3
@@ -13,3 +14,15 @@ json_t* encode_semba_mail_gear_request(
 json_t *createUpdateColorReq(CharHairColor& charHairColor);
 
 std::string createSaveReq(const char* saves_dir, const char *name);
+
+std::pair<int, std::string> createListSaveFilesRequest(const std::string& savesDir);
+
+std::pair<int, std::string> wrapSkipTutorial(bool skipTutorial);
+
+std::pair<int, std::string> parseGetSkipTutorialResponse(std::string response, bool& skipTutorial);
+
+std::pair<int, std::string> parseListSaveFilesResponse(
+    const std::string& response, std::vector<std::string>& save_files
+);
+
+void updateHairColorMapWithHairColors(CharHairColorMap& hairColorMap, json_t *hair_colors);
